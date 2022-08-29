fe0f2a2132cd16be27d51e4f91a2fb661d39bd6a
ivinercassio <ivinercassio@gmail.com>
2022-08-29 18:10:51 -0300

    Crud User (29-08)

--- src/main/java/br/com/ivinerejose/plusAds/PlusAdsApplication.java
+++ src/main/java/br/com/ivinerejose/plusAds/PlusAdsApplication.java
@@ -1,13 +1,30 @@
 package br.com.ivinerejose.plusAds;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+import br.com.ivinerejose.plusAds.model.User;
+import br.com.ivinerejose.plusAds.model.repository.UserRepository;
+
 
 @SpringBootApplication
-public class PlusAdsApplication {
+public class PlusAdsApplication implements ApplicationRunner{
+
+	@Autowired
+	UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlusAdsApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args){
+		User user = new User("cnpj", "email@teste.com", "123");
+		user = userRepository.save(user); // retorna o mesmo pet, mas agr com o id
+		System.out.println(user);		
 	}
 
 }
