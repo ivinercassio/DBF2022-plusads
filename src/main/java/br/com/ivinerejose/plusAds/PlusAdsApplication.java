3b9cfd78cf38f86e4ae14cb9c97c6e7ed490a18c
ivinercassio <ivinercassio@gmail.com>
2022-10-15 13:04:21 -0300

    Api Anunciante (15-10)

--- src/main/java/br/com/ivinerejose/plusAds/PlusAdsApplication.java
+++ src/main/java/br/com/ivinerejose/plusAds/PlusAdsApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
-import br.com.ivinerejose.plusAds.controller.api.ContratanteController;
+import br.com.ivinerejose.plusAds.model.Anunciante;
 import br.com.ivinerejose.plusAds.model.Contratante;
+import br.com.ivinerejose.plusAds.model.repository.AnuncianteRepository;
 import br.com.ivinerejose.plusAds.model.repository.ContratanteRepository;
 
 
@@ -17,7 +18,7 @@ public class PlusAdsApplication implements ApplicationRunner{
 	@Autowired
 	ContratanteRepository contratanteRepository;
 	@Autowired
-	ContratanteController contratanteController;
+	AnuncianteRepository anuncianteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlusAdsApplication.class, args);
@@ -31,25 +32,10 @@ public class PlusAdsApplication implements ApplicationRunner{
 		contratante = contratanteRepository.save(contratante); // retorna o mesmo contratante, mas agr com o id
 		System.out.println("Contratante: " + contratante);	
 
-		// editando o primeiro
-		// System.out.println("Vamos editar isso...");
-		// contratante.setEmail("email@contratante.com");
-		// System.out.println("Editado: " + contratanteController.update(contratante));
-		
-		// Contratante contratante2 = new Contratante("Lenovo", "08912345715", null, 
-		// 										"lenovo@contratante.com", "123",
-		// 										"A", "31994578461", null);
-		// contratante2 = contratanteRepository.save(contratante2); // retorna o mesmo contratante, mas agr com o id
-		// System.out.println("Contratante: " + contratante2);	
-
-		// excluindo o segundo 
-		// System.out.println("Vamos excluir o segundo contratante...");
-		// System.out.println("Excluido: " + contratanteController.deleteContratante(contratante2.getId()));
-		// if(contratante2 != null){
-		// 	System.out.println("Contratante 2: " + contratante2);
-		// }else{
-		// 	System.out.println("Registro excluido com sucesso!!");
-		// }
+		Anunciante anunciante = new Anunciante("Antonio Fagundes", "14472362607", null, "antonioFagundes@gmail.com", "123",
+										"A", "31980210630", "31932837581");
+		anunciante = anuncianteRepository.save(anunciante); // retorna o mesmo anunciante, mas agr com o id
+		System.out.println(anunciante);	
 	}
 
 }
--- src/main/java/br/com/ivinerejose/plusAds/model/repository/ContratanteRepository.java
+++ src/main/java/br/com/ivinerejose/plusAds/model/repository/ContratanteRepository.java
@@ -1,10 +1,16 @@
 package br.com.ivinerejose.plusAds.model.repository;
 
+// import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.ivinerejose.plusAds.model.Contratante;
 
 @Repository
 public interface ContratanteRepository extends JpaRepository<Contratante, Long>{
+
+    // @Query("select c from Contratante c where c.email = :email and c.senha = :senha")
+    // public Optional<Contratante> buscarLogin(String email, String senha);
 }
